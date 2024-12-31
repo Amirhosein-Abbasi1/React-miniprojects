c022973f9e7526f68495183e2dea1a8ee9c6e489
Amirhosein Abbasi <amirhabbasi2007@gmail.com>
2024-12-31 16:28:19 +0330

    Part #2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React  from "react";
 import {createRoot} from "react-dom/client";
 
 const time = () =>{
@@ -12,8 +12,50 @@ const time = () =>{
         </h2>
     </div>
     )
-    createRoot(document.getElementById("root")).render(elem);
+    //createRoot(document.getElementById("root")).render(elem);
 }
 setInterval(
     ()=>{time();}, 1000
+)
+
+
+
+//Component
+class App1 extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>
+                what time is it?
+            </h1>
+        </div>
+        )
+    }
+}
+class App2 extends React.Component{
+    render(){
+        return(
+            <div>
+            <h2>
+                time is : {new Date().toLocaleTimeString()}
+               </h2>
+            </div>   
+        )
+    }
+}
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <App1/>
+                <App2/>
+            </div>
+        )
+    }
+}
+const timer = () =>{
+    createRoot(document.getElementById("root")).render(<App/>);
+}
+setInterval(()=> {
+    timer();}, 1000
 )
